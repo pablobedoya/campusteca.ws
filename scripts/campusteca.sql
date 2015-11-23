114336ef9a212e37c702c1d4cce281189bbf049d
josielmoreira <josielmoreira.eng@gmail.com>
2015-11-23 18:12:03 -0300

    adding scripts database

---
+++ scripts/campusteca.sql
@@ -0,0 +1,32 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema campusteca
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema campusteca
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `campusteca` DEFAULT CHARACTER SET utf8 ;
+USE `campusteca` ;
+
+-- -----------------------------------------------------
+-- Table `campusteca`.`review`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `campusteca`.`review` (
+  `idreview` INT NOT NULL,
+  `idbook` INT NOT NULL,
+  `iduser` INT NOT NULL,
+  `rating` INT NOT NULL,
+  `description` VARCHAR(255) NULL,
+  PRIMARY KEY (`idreview`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
